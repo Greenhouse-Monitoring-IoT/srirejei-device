7471ba5bb9182788132a97d00c8c3409fdbe8793
naufalmuafi <nmuafi1@gmail.com>
2022-06-03 02:52:22 +0700

    add program to control device

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from machine import Pin, ADC
+from machine import Pin, ADC, SoftI2C
 from time import sleep
+from lcd_api import LcdApi
+from i2c_lcd import I2cLcd
 import dht
 
 class LDR:    
@@ -26,28 +28,61 @@ r_led = Pin(33, Pin.OUT)
 g_led = Pin(32, Pin.OUT)
 DHT = dht.DHT22(Pin(14))
 
-while True:
-    ldr_value = ldr.value()
-    print('value = {}'.format(ldr_value))
-    sleep(1)
-    
-    
-    
-    if ldr_value > 30 and ldr_value < 100:
-        r_led.value(1)
-    else:
-        r_led.value(0)
-        
+I2C_ADDR = 0x27
+rows = 2
+columns = 16
+i2c = SoftI2C(scl=Pin(22), sda=Pin(21), freq=10000)
+lcd = I2cLcd(i2c, I2C_ADDR, rows, columns)
+
+def ldr_condition(val):
     try:
-        sleep(2)
-        sensor.measure()
-        temp = sensor.temperature()
-        hum = sensor.humidity()
+        if val > 30 and val < 100:
+            r_led.value(1)
+        else:
+            r_led.value(0)
+    except:
+        print("Sensor LDR bermasalah")
+
+def temp_codition(temperature):
+    try:
+        if temperature > 25 and temperature < 32:
+            g_led.value(1)
+        else:
+            g_led.value(0)
+    except:
+        print('Sensor DHT bermasalah!')
+
+def printMonitoring(tempt, tempt_f, humi, ldrVal):
+    print(f'Temperature: {tempt:.2f} C | {tempt_f:.2f} F')
+    lcd.putstr(f"Temperature: str({tempt:.2f}) C | str({tempt_f:.2f}) F")
+    sleep(3)
+    lcd.clear()
+    
+    print(f'Humidity: {humi:.2%}')
+    lcd.putstr(f"Humidity: str({humi:.2%})")
+    sleep(3)
+    lcd.clear()
+    
+    print(f'LDR Value: {ldrVal}')
+    lcd.putstr(f"LDR Value: str({ldrVal})")
+    sleep(3)
+    lcd.clear()
+
+while True:    
+    sleep(2)
+    lcd.putstr("Selamat Datang!        Menginisialisasi...")
+    sleep(3)
+    lcd.clear()
+    try:        
+        DHT.measure()
+        temp = DHT.temperature()
+        hum = DHT.humidity()
         temp_f = temp * (9/5) + 32.0
         ldr_value = ldr.value()
-        print(f'Temperature: {temp} C or {temp} F')
-        print(f'Humidity: {hum}')
-        print(f'LDR Value: {ldr_value}')
+                
+        printMonitoring(temp, temp_f, hum, ldr_value)
+        ldr_condition(ldr_value)
+        temp_condition(temp)
         
-    except OSError as e:
+    except DeviceError as e:
         print('Gagal untuk memuat alat, silakan muat ulang!')
